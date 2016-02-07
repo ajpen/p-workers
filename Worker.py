c29b7fe5a5b68df1076b8d1e8a7c1f3ecf61d310
ajpen <anferneeisthebest@gmail.com>
2016-02-07 19:50:13 -0400

    added worker module

---
+++ Worker.py
@@ -0,0 +1,82 @@
+import multiprocessing
+import Queue
+import os
+import inspect
+
+
+class WorkerClass(multiprocessing.Process):
+
+	## WorkerClass Core ##
+
+
+	# initialize parent ID and global task queue
+	def __init__(self, jobQueue, WorkerFunctions):
+		multiprocessing.Process.__init__(self) # initialize process
+		self.jobQueue = jobQueue 			   # set up the queue for receiving jobs
+		self.WorkerFunctions = WorkerFunctions # sets up the functions available to the worker
+		self.parentPID = os.getpid()           # sets the parent pid
+
+	
+	# checks to see if parent is still alive. This prevents the accumulation of orphaned processes and simplifies cleanup
+	def is_orphaned(self):
+		try:
+			os.kill(self.parentPID, 0)
+		except OSError:
+			return True
+
+		return False
+
+
+	# jobHandler parses query and runs the job's target function
+	def jobHandler(self, job):
+
+		# runs job with query
+		self.WorkerFunctions[job["target"]](job["query"])
+
+
+	# starts worker
+	def run(self):
+		jobQueue = self.jobQueue
+		while True:
+
+			# check if its time to commit seppuku (kill itself if parent pid changes)
+			if self.is_orphaned():
+				raise SystemExit
+
+			# check queue every second
+			try:
+				job = jobQueue.get(True,1)
+			except Queue.Empty:
+				continue
+
+			# if worker got a job, handle the job
+			self.jobHandler(job)
+
+
+
+# starts count workers and returns the job queue used by the workers
+def StartWorkers(count, attributeClass):
+
+	# initialize job queue
+	JobQueue = multiprocessing.Queue()
+
+	workers = [ WorkerClass(JobQueue, generateAttributes(attributeClass)) for i in xrange(count) ]
+	
+	for worker in workers:
+		worker.setDaemon = True
+		worker.start()
+
+	return JobQueue
+
+
+# generate an attribute dictionary from a class of methods
+def generateAttributes(attrclass):
+
+	try:
+		attrList = inspect.getmembers(attrclass, predicate=inspect.ismethod)
+		return {key:value for (key, value) in attrList}
+	
+	# if failed, print message and exit
+	except:
+		print "ERROR: failed to generate attributes from Class. Exiting..."
+		raise SystemExit
